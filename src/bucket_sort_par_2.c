bd226e9add5b236d18d0fe2dbc5ffcda1e1e5f0d
Adam K <adamklekowski@protonmail.com>
2023-04-18 20:03:52 +0200

    final version

--- src/bucket_sort_par_2.c
+++ src/bucket_sort_par_2.c
@@ -3,16 +3,20 @@
 #include <stdlib.h>
 #include <time.h>
 
+typedef int bool;
+#define true 1
+#define false 0
+
 #ifndef N
-#define N 1000000
+#define N 10
 #endif /*N*/
 
 #ifndef B
-#define B 4
+#define B 2
 #endif /*B*/
 
 #ifndef P
-#define P 4
+#define P 2
 #endif /*P*/
 
 #ifndef T
@@ -22,8 +26,7 @@
 
 float* init_array(int n) {
     float* result = (float*) malloc(n * sizeof(float));
-    int i;
-    for (i = 0; i < n; i++){
+    for (int i = 0; i < n; i++){
         result[i] = 0;
     }
     return result;
@@ -31,11 +34,25 @@ float* init_array(int n) {
 
 float** init_2d_array(int n, int m) {
     float** result = (float**) malloc(n * sizeof(float*));
-    int i;
-    for (i = 0; i < n; i++){
+    for (int i = 0; i < n; i++){
         result[i] = (float*) malloc(m * sizeof(float));
     }
     return result;
+}
+
+omp_lock_t* init_bucket_locks() {
+    omp_lock_t* locks = (omp_lock_t*) malloc(B * sizeof(omp_lock_t));
+    for (int i = 0; i < B; i++){
+        omp_init_lock(&locks[i]);
+    }
+    return locks;
+}
+
+void free_bucket_locks(omp_lock_t* bucket_locks) {
+    for (int i = 0; i < B; i++){
+        omp_destroy_lock(&bucket_locks[i]);
+    }
+    free(bucket_locks);
 }
 
 void free_2d_array(float** arr, int n) {
@@ -47,7 +64,6 @@ void free_2d_array(float** arr, int n) {
 
 int* init_ints(int n) {
     int* result = (int*) malloc(n*sizeof(int));
-
     int i;
     for (i = 0; i < n; i++) {
         result[i] = 0;
@@ -70,74 +86,57 @@ void fill_array_randomly(float* arr, int n, int tid) {
     xi[1] = tid + 2 + sec;
     xi[2] = tid + 1 + sec;
 
-    #pragma omp for private(i) schedule(guided)
+#pragma omp for private(i) schedule(guided)
     for (i = 0; i < n; i++){
         arr[i] = erand48(xi);
     }
 }
 
-
-int compute_buck_start_ind(int thr){
-    float step = (float) B / (float) P;
-    int result = (int) ((float) thr * step);
-    return result;
-}
-
-int compute_buck_end_ind(int thr){
-    if (thr == P - 1) return B-1;
-    return compute_buck_start_ind(thr + 1) - 1;
-}
-
-
-void distribute_to_buckets(float* arr, float** buckets, int* bucket_ind){
+void distribute_to_buckets(const float* arr, float** buckets, int* bucket_ind,  omp_lock_t* bucket_locks) {
     int i;
-    #pragma omp for private(i) schedule(guided)
+    int number_of_elements_in_bucket;
+#pragma omp for private(i, number_of_elements_in_bucket) schedule(guided)
     for (i = 0; i < N; i++) {
         int number_of_bucket = (int) B*arr[i];
-        #pragma omp critical
-        {
-            int number_of_elements_in_bucket = bucket_ind[number_of_bucket];
-            buckets[number_of_bucket][number_of_elements_in_bucket] = arr[i];
-            bucket_ind[number_of_bucket]++;
-        }
+
+        omp_set_lock(&bucket_locks[number_of_bucket]);
+        number_of_elements_in_bucket = bucket_ind[number_of_bucket];
+        buckets[number_of_bucket][number_of_elements_in_bucket] = arr[i];
+        bucket_ind[number_of_bucket]++;
+        omp_unset_lock(&bucket_locks[number_of_bucket]);
     }
 }
 
-void sort_buckets(float** buckets, int* bucket_ind, int start_ind, int end_ind) {
-    for (int i = start_ind; i <= end_ind; i++){
+void sort_buckets(float** buckets, int* bucket_ind) {
+    int i;
+#pragma omp for private(i) schedule(guided)
+    for (i = 0; i < B; i++){
         qsort(buckets[i], bucket_ind[i], sizeof(*(buckets[i])), asc);
     }
 }
 
-void merge_buckets(float* arr, float** buckets, const int* bucket_ind, int bucket_start_ind, int from, int to){
-    int bucket_num = bucket_start_ind;
-    int i = from;
-    while (i < to) {
-        for (int j = 0; j < bucket_ind[bucket_num]; j++) {
-            arr[i] = buckets[bucket_num][j];
-            i++;
+void merge_buckets(float* arr, float** buckets, const int* bucket_ind, const int* cummulative_sum_arr) {
+    int i, j, arr_i;
+#pragma omp for private(i, j, arr_i) schedule(guided)
+    for (i=0; i < B; i++) {
+        arr_i = i == 0 ? 0 : cummulative_sum_arr[i-1];
+        for (j=0; j < bucket_ind[i]; j++) {
+            arr[arr_i] = buckets[i][j];
+            arr_i ++;
         }
-        bucket_num++;
     }
 }
 
-void print_float_arr(float* arr, int n){
-    for (int i = 0; i < n; i++) {
-        printf("%4f, ", arr[i]);
-    }
-    printf("\n");
-}
-
-int is_sorted(const float* arr, int n){
+bool is_sorted(const float* arr, int n) {
     for (int i = 1; i < n; i++){
         if (arr[i-1] > arr[i]){
-            return 0;
+            return false;
         }
     }
-    return 1;
+    return true;
 }
 
-double avg(const double* arr, int n){
+double avg(const double* arr, int n) {
     double sum = 0;
     for (int i = 0; i < n; i++){
         sum += arr[i];
@@ -145,48 +144,50 @@ double avg(const double* arr, int n){
     return sum / (float) n;
 }
 
+
+int* calculate_cumulative_sum(const int* arr, int n, int *cummulative_sum_arr) {
+    int i;
+    cummulative_sum_arr[0] = arr[0];
+    for (i = 1; i < n; i++){
+        cummulative_sum_arr[i] = cummulative_sum_arr[i-1] + arr[i];
+    }
+    return cummulative_sum_arr;
+}
+
 int main() {
     omp_set_num_threads(P);
 
     float* arr = init_array(N);
-    float** buckets = init_2d_array(B, 5*(N/B));
+    float** buckets = init_2d_array(B, 3*(N/B));
     int* bucket_ind = init_ints(B);
+    omp_lock_t* bucket_locks = init_bucket_locks();
+    int *cummulative_sum_arr = init_ints(B);
 
     double* t_a_arr = malloc(P*sizeof(double));
     double* t_b_arr = malloc(P*sizeof(double));
     double* t_c_arr = malloc(P*sizeof(double));
 
     double t_start = omp_get_wtime();
-    #pragma omp parallel
+#pragma omp parallel
     {
         int tid = omp_get_thread_num();
 
         fill_array_randomly(arr, N, tid);
         t_a_arr[tid] = omp_get_wtime();
 
-        distribute_to_buckets(arr, buckets, bucket_ind);
+        distribute_to_buckets(arr, buckets, bucket_ind, bucket_locks);
         t_b_arr[tid] = omp_get_wtime();
 
-        int buck_start_ind = compute_buck_start_ind(tid);
-        int buck_end_ind = compute_buck_end_ind(tid);
-        sort_buckets(buckets, bucket_ind, buck_start_ind, buck_end_ind);
-        #pragma omp barrier
+        sort_buckets(buckets, bucket_ind);
         t_c_arr[tid] = omp_get_wtime();
 
-        int merge_start_ind = 0;
-        for (int i = 0; i < buck_start_ind; i++) {
-            merge_start_ind += bucket_ind[i];
-        }
-        int merge_end_ind = merge_start_ind;
-        for (int i = buck_start_ind; i <= buck_end_ind; i++) {
-            merge_end_ind += bucket_ind[i];
-        }
-        merge_buckets(arr, buckets, bucket_ind, buck_start_ind, merge_start_ind, merge_end_ind);
+#pragma omp single
+        cummulative_sum_arr = calculate_cumulative_sum(bucket_ind, B, cummulative_sum_arr);
+        merge_buckets(arr, buckets, bucket_ind, cummulative_sum_arr);
     }
     double t_end = omp_get_wtime();
 
-    int sorted = is_sorted(arr, N);
-
+    bool sorted = is_sorted(arr, N);
     double t_a_delta = avg(t_a_arr, P) - t_start;
     double t_b_delta = avg(t_b_arr, P) - avg(t_a_arr, P);
     double t_c_delta = avg(t_c_arr, P) - avg(t_b_arr, P);
@@ -198,6 +199,7 @@ int main() {
     free(arr);
     free(bucket_ind);
     free_2d_array(buckets, B);
+    free_bucket_locks(bucket_locks);
     free(t_a_arr);
     free(t_b_arr);
     free(t_c_arr);
